281826b189c4ffa219fb174cf9e539337d357ba5
Ishan Garg <ishan0407@gmail.com>
2018-09-18 17:49:18 +0530

    Update Compute.py

--- Compute.py
+++ Compute.py
@@ -1,97 +1,37 @@
-import Course
-import Instructor
-import Student
-#Returns lambda_c for a single course
-def lambda_c(course, student_list): #course is an object of course class, student_list is a list of Students
-    c1=0
-    c2=0
-    c3=0
-    for j in student_list: 
-        for i in j.preferences: #Checking priority for particular course.
-            if(i[0]==course.course_code):
-                c1=c1+1
-            elif(i[1]==course.course_code):
-                c2=c2+1
-            elif(i[2]==course.course_code):
-                c3=c3+1
-        
-    app=3*c1+2*c2+1*c3 #Calculating lambda_c
-    lam_c=app/course.size_remaining
+from heapq import heappush,heappop
+class Course:
+	applied=[]
+	selected=[]
+	def __init__(self, course_code, size_remaining, selected, applied):
+		self.course_code=course_code
+		self.size_remaining=size_remaining
+		self.selected=selected
+		self.applied=applied
 
-    return lam_c;
+	def addtoApplied(student,lambda_s):
+		heappush(self.applied,(-lambda_s,student))
 
-def lambda_s(student, p): #Returns lambda_s
-    return (student.preferences[0][1]*0.8+student.CG*0.2);
-    
-def instructorAllotPreference(instructor_list, course_list): #Not checking for clashes, not checking if student applied for the course.
-    #List of all instructors, list of all courses.
-    for i in instructor_list:
-        for k, l in i.preference:
-            l.alloted=k.course_code;
-            l.allotCourse(k.course_code)
-            k.size_remaining=k.size_remaining-1;
+	def popApplied():
+		return heappop(self.applied)
 
-def fillAppliedFirstPreference(course_list, student_list):
-#List of all courses, list of all students
-    for i in student_list:
-        for j in course_list:
-            if(i.preferences[0][0]==j.course_code):
-                ls=lamda_s(i, 0);
-                heapq.heappush(j.applied,(-ls, i));
+	def appliedRemaining():
+		return length(self.applied)>0
 
-def sortCourses(course_list, student_list):
-#List of all courses, list of all students
-	 lambda_c_list=[];
-	 for i in course_list:
-	 	 c=lambda_c(i, student_list); 
-	 	 lambda_c_list.append((i, c));
-	 	 
-	 sorted(lambda_c_list, key=getKey);
-	 return lambda_c_list;
+	def decrementSize():
+		self.size_remaining-=1
 
+	def isFull():
+		return self.size_remaining==0
 
+	def allot(student,lambda_s):
+		heappush(self.selected,(lambda_s,student))
+		decrementSize()
 	
-def getKey(item):
-	    return item[1];
+	def getWorstSelected():
+		return self.selected[0]
 
-def allot(course):
+	def popWorstSelected():
+		return heappop(self.selected)
 
-    while(!course.full() and course.appliedRemaining()):
-        most_deserving=course.popApplied()
-        course.allot(most_deserving)
-        most_deserving[1].allotCourse(course.getCourseCode())
-    
-    fillRemaining(course)
-
-        
-def fillRemaining(course):
-    while(course.appliedRemaining()): 
-        most_deserving=course.popApplied();
-        nextPreference(-most_deserving[0], most_deserving[1], course);
-
-def nextPreference(ls, student, course):
-        most_deserving=(ls, student);
-        flag=false
-        for k, l in most_deserving:
-            for j in l.preferences:
-                if(j[0]==course): #If course matches
-                    f=true
-                elif (f):
-                    ls=lamda_s(l, j);
-                    if(j[0].size_remaining!=0): #And next preference is not full                        
-                        heapq.heappush(j[0].applied, (-ls, l)) #Push student into applied of next preference
-                    else:
-                        ifFull(ls, student,course)
-                    break
-
-def ifFull(ls, student, course):
-    worst_student=heapq.heappop(course.selected);
-    for k, l in worst_student:
-        if(k<ls):
-            heapq.heappush(course.selected, (ls, student));
-            l.alloted="";
-            student.alloted=course.course_code;
-            nextPreference(k, l, course);
-        else:
-            heapq.heappush(course.selected, worst_student);
-            nextPreference(ls, student, course);
+	def getCourseCode():
+		return self.course_code
