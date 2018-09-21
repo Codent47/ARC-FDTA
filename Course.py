bcfc6569c82151b84cc27cee1cfbd6f33ae84bd0
KeerthanaSrikanth <keerthanasrikanth05@gmail.com>
2018-09-21 12:09:54 +0530

    Add self to methods

--- Course.py
+++ Course.py
@@ -2,37 +2,35 @@ from heapq import heappush,heappop
 class Course:
 	applied=[]
 	selected=[]
-	def __init__(self, course_code, size_remaining, selected, applied):
+	def __init__(self, course_code, size_remaining):
 		self.course_code=course_code
 		self.size_remaining=size_remaining
-		self.selected=selected
-		self.applied=applied
-
-	def addtoApplied(student,lambda_s):
+		
+	def addtoApplied(self, student,lambda_s):
 		heappush(self.applied,(-lambda_s,student))
 
-	def popApplied():
+	def popApplied(self):
 		return heappop(applied)
 
-	def appliedRemaining():
+	def appliedRemaining(self):
 		return length(self.applied)>0
 
-	def decrementSize():
+	def decrementSize(self):
 		self.size_remaining-=1
 
-	def allot(student,lambda_s):
+	def allot(self, student,lambda_s):
 		heappush(self.selected,(lambda_s,student))
 		decrementSize()
 	
-	def getWorstSelected():
+	def getWorstSelected(self):
 		return selected[0]
 
-	def popWorstSelected():
+	def popWorstSelected(self):
 		self.size_remaining-=1
 		return heappop(selected)
 
-	def getCourseCode():
+	def getCourseCode(self):
 		return self.course_code
 
-	def getSizeRemaining():
+	def getSizeRemaining(self):
 		return self.size_remaining
