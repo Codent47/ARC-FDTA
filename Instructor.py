7bc56efa83c9a7dc7fa854f9efcf5e603c63f50b
KeerthanaSrikanth <keerthanasrikanth05@gmail.com>
2018-09-21 12:10:57 +0530

    Add self to methods

--- Instructor.py
+++ Instructor.py
@@ -3,5 +3,5 @@ class Instructor:
 	def __init__(self, preference):
 		self.preference=preference #List of pairs (Course, Student)
 
-	def getPreferences():
+	def getPreferences(self):
 		return self.preference
