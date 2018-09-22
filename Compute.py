3edc28b7659af2e94b2c17605f568348836604cc
Ishan Garg <ishan0407@gmail.com>
2018-09-22 16:45:42 +0530

    Update Compute.py

--- Compute.py
+++ Compute.py
@@ -5,26 +5,22 @@ import operator
 import pandas as pd
 #Returns lambda_c for a single course
 def lambda_c(course, student_list): #course is an object of course class, student_list is a list of Students
-    c1=0
-    c2=0
-    c3=0
-    for j in student_list: 
+    c=[0,0,0,0,0]
+    for j in student_list:
+        cnt=0 
         for i in j.getPreferences(): #Checking priority for particular course.
-            if(i[0]==course.getCourseCode()):
-                c1=c1+1
-            elif(i[1]==course.getCourseCode()):
-                c2=c2+1
-            elif(i[2]==course.getCourseCode()):
-                c3=c3+1
+            if (i[0]==course.getCourseCode()):
+                c[cnt]+=1
+            cnt+=1
         
-    app=3*c1+2*c2+1*c3 #Calculating lambda_c
+    app=5*c[0]+4*c[1]+3*c[2]+2*c[3]+c[4] #Calculating lambda_c
     lam_c=app/course.getSizeRemaining()
 
     return lam_c
 
 def lambda_s(student, grade): #Returns lambda_s
-    
-    return (grade*0.8+student.getCG()*0.2)
+    grade=float(grade)
+    return (grade*10+float(student.getCG()))
     
 def instructorAllotPreference(instructor_list, course_list): #Not checking for clashes, not checking if student applied for the course.
     #List of all instructors, list of all courses.
@@ -36,11 +32,11 @@ def instructorAllotPreference(instructor_list, course_list): #Not checking for c
 def fillAppliedFirstPreference(course_list, student_list):
 #List of all courses, list of all students
     for i in student_list:
-        for j in course_list:
-            x=i.getPreferences()
-            if(x[0][0]==j.getCourseCode()):
-                ls=lambda_s(i, x[0][1])
-                j.addToApplied(i, ls)
+        if (i.getAllotedCourse()!="None"):
+            continue
+        x=i.getPreferences()
+        ls=lambda_s(i, x[0][1])
+        course_code_dict[x[0][0]].addToApplied(i, ls)
 
 def sortCourses(course_list, student_list):
 #List of all courses, list of all students
@@ -53,11 +49,6 @@ def sortCourses(course_list, student_list):
      lambda_c_list=[course for course, lam_c in lambda_c_list]
      return lambda_c_list
 
-
-    
-def getKey(item):
-        return item[1]
-
 def allot(course):
     while(course.getSizeRemaining()>0  and course.appliedRemaining()>0):
         most_deserving=course.popApplied()
@@ -69,25 +60,21 @@ def allot(course):
         
 def fillRemaining(course):
     while(course.appliedRemaining()>0): 
-        most_deserving=course.popApplied()
-               
-        nextPreference(-most_deserving[0], most_deserving[1], course)
+        most_deserving=course.popApplied()               
+        nextPreference(most_deserving[1], course)
 
-def nextPreference(ls, student, course):
-        most_deserving=(ls, student)
-        flag=false
-        for k, l in most_deserving:
-            for j in l.getPreferences():
-                if(j[0]==course): #If course matches
-                    flag=true 
-                elif(flag):
-                    ls=lambda_s(l, j[1])
-                    if(j[0].getSizeRemaining()>0): #And next preference is not full 
-                        j[0].addToApplied(l, ls) #Push student into applied of next preference                     
-                     
-                    else:
-                        ifFull(ls, student, j[0]) #Check this line. Changed it
-                    break
+def nextPreference(student, course):
+        flag=False
+        for j in student.getPreferences():
+            if(j[0]==course): #If course matches
+                flag=True 
+            elif(flag):
+                ls=lambda_s(student, j[1])
+                if(j[0].getSizeRemaining()>0): #And next preference is not full 
+                    j[0].addToApplied(l, ls) #Push student into applied of next preference                     
+                else:
+                    ifFull(ls, student, j[0]) #Check this line. Changed it
+                break
 
 def ifFull(ls, student, course):
     worst_student=getWorstSelected()
@@ -112,8 +99,41 @@ df=pd.read_csv('Student.csv')
 student_list=[]
 student_id_dict={}
 for index, row in df.iterrows():
+    if (row['ID'] in student_id_dict): #to take care of duplicate data set
+        continue                        
     prefs=[(row['Course1'],row['Grade1']),(row['Course2'],row['Grade2']),(row['Course3'],row['Grade3']),(row['Course4'],row['Grade4']),(row['Course5'],row['Grade5'])]
     s=Student(row['ID'],prefs,"None",row['CG'])
     student_list.append(s)
     student_id_dict[s.getID()]=s
 
+df=pd.read_csv('Instructor.csv')
+instructor_list=[]
+instructor_id_dict={}
+for index, row in df.iterrows():
+    c=course_code_dict[row['Course']]
+    s=student_id_dict[row['Student']]
+    prefs=[(c,s)]
+    if (row['Instructor'] in instructor_id_dict):
+        instructor_id_dict[row['Instructor']].addToPreferences(c,s)
+    else:
+        i=Instructor(prefs)
+        instructor_list.append(i)
+        instructor_id_dict[row['Instructor']]=i
+
+instructorAllotPreference (instructor_list,course_list)
+
+for c in course_list:
+    if (c.getSizeRemaining()==0):
+        course_list.remove(c)
+
+course_list=sortCourses(course_list,student_list)
+
+fillAppliedFirstPreference(course_list,student_list)
+
+for c in course_list:
+    allot(c)
+
+print(len(student_list))
+
+for s in student_list:
+    print(s.getID(),s.getAllotedCourse())
