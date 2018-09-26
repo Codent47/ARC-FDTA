5a3efa1942d9eb821a8b7080ed80a3eddef7d993
harshrawat17 <33692444+harshrawat17@users.noreply.github.com>
2018-09-26 20:02:02 +0530

    course_document.py

--- Course.py
+++ Course.py
@@ -1,36 +1,50 @@
 from heapq import heappush,heappop
 class Course:
-	applied=[]
-	selected=[]
 	def __init__(self, course_code, size_remaining):
 		self.course_code=course_code
 		self.size_remaining=size_remaining
-		
-	def addtoApplied(self, student,lambda_s):
+		self.applied=[]
+		self.selected=[]
+	
+	#adding students to the applied list
+	def addToApplied(self, student,lambda_s):
 		heappush(self.applied,(-lambda_s,student))
+    
+    #prints the student ID no.s who have applied
+	def printApplied(self):
+		for x in self.applied:
+			print(x[1].getID())
 
+	#pops the best student suitable for the course on the basis of lambda_s
 	def popApplied(self):
-		return heappop(applied)
+		return heappop(self.applied)
 
+    #checking the number of students in the applied list
 	def appliedRemaining(self):
-		return length(self.applied)>0
+		return len(self.applied)
 
+	#decrements the size of the course list
 	def decrementSize(self):
 		self.size_remaining-=1
 
+	#pushing the worst student (on basis of lambda_s) to the top of the list
 	def allot(self, student,lambda_s):
 		heappush(self.selected,(lambda_s,student))
-		decrementSize()
+		self.decrementSize()
 	
+	#returns the worst student
 	def getWorstSelected(self):
-		return selected[0]
+		return self.selected[0]
 
+    #removes the worst student from the selected list
 	def popWorstSelected(self):
 		self.size_remaining-=1
-		return heappop(selected)
+		return heappop(self.selected)
 
+	#returns the course code of the course
 	def getCourseCode(self):
 		return self.course_code
 
+	#returns the size remaining in the course
 	def getSizeRemaining(self):
 		return self.size_remaining
