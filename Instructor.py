2ef525810185f062390556103c1ebb8d6c209c19
harshrawat17 <33692444+harshrawat17@users.noreply.github.com>
2018-09-26 20:03:41 +0530

    document_instructor.py

--- Instructor.py
+++ Instructor.py
@@ -1,7 +1,12 @@
 class Instructor:
 	
 	def __init__(self, preference):
-		self.preference=preference #List of pairs (Course, Student)
+		self.preference=preference #here preference is a list of pairs (Course, Student)
 
+	#list of students given by the instructors of their respective courses:-
 	def getPreferences(self):
 		return self.preference
+
+	#adding students into the above preference list:-
+	def addToPreferences(self,course,student):
+		self.preference.append((course,student))
